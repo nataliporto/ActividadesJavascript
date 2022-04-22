b2f1daf55e1c4c87f972442b14fa65b10bcf219a
Natalí Porto <nataliporto@gmail.com>
2022-04-22 16:09:12 -0300

    1er Commit

---
+++ index.js
@@ -0,0 +1,24 @@
+/*Escribe un programa de una sola línea que haga que 
+aparezca en la pantalla un alert que diga “Hello World”*/
+alert("Hello World");
+
+/*Escribe un programa de una sola línea que escriba en 
+la pantalla un texto que diga “Hello World”*/
+document.write("Hello World");
+
+/*Escribe un programa de una sola línea que escriba en 
+la pantalla el resultado de sumar 3 + 5 */
+document.write(3+5);
+
+/*Escribe un programa de dos líneas que pida el nombre del 
+usuario con un prompt y escriba un texto que diga “Hola nombreUsuario” */
+var nom=prompt("Ingrese su nombre de usuario");
+document.write("Hola ", nom);
+
+/*Escribe un programa de tres líneas que pida un número, 
+pida otro número y escriba el resultado de sumar estos 
+dos números*/
+var num1=parseInt(prompt("Ingrese un número"));
+var num2=parseInt(prompt("Ingrese otro número"));
+document.write(num1 + num2);
+
